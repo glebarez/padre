47e04813534b292e17da5ed51d4ba133cd6d0425
glebarez <gleb.sakhnov@gmail.com>
2020-04-15 20:36:05 +0200

    test cases : lenghty inputs, no redirections

--- test_server/test_b64.sh
+++ test_server/test_b64.sh
@@ -1,1 +1,1 @@
-cat testCiphers | go run ../... -u "http://localhost:5000/decrypt?cipher=$" -err IncorrectPadding -p 200 -post "cipher=$" -cookie "token=$" "$@" > out
+cat testCiphers | go run ../... -u "http://localhost:5000/decrypt?cipher=$" -err IncorrectPadding -p 200 -post "cipher=$" -cookie "token=$" "$@"
--- test_server/test_lhex_enc.sh
+++ test_server/test_lhex_enc.sh
@@ -1,1 +1,1 @@
-cat testPlains | go run ../... -u "http://localhost:5000/decrypt?cipher=$&enc=lhex" -err IncorrectPadding -p 200 -post "cipher=$" -cookie "token=$" -e lhex -enc "$@" > out
+cat testPlains | go run ../... -u "http://localhost:5000/decrypt?cipher=$&enc=lhex" -err IncorrectPadding -p 200 -post "cipher=$" -cookie "token=$" -e lhex -enc "$@"
